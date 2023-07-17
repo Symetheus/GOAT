7d7e7d6ad131bfb8dcefd5742ff8478f13970c3e
Elsa <elsafirmin@hotmail.fr>
2023-07-17 20:28:03 +0200

    [US-30][UC-31] Get user information

--- app/src/main/java/com/example/goat/presentation/profile/UserProfile.kt
+++ app/src/main/java/com/example/goat/presentation/profile/UserProfile.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -39,64 +40,78 @@ fun UserProfile(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        if (uiState.value.user?.photo?.isNotEmpty() == true) {
-            Image(
-                painter = rememberAsyncImagePainter(model = uiState.value.user!!.photo),
-                contentDescription = "Profile Picture",
-                modifier = Modifier
-                    .size(120.dp)
-                    .clip(shape = CircleShape)
-            )
+        if (uiState.value.isLoading) {
+            LoadingIndicator()
         } else {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                contentDescription = "Profile Picture",
-                modifier = Modifier
-                    .size(120.dp)
-                    .clip(shape = CircleShape)
-            )
-        }
+            if (uiState.value.user?.photo?.isNotEmpty() == true) {
+                Image(
+                    painter = rememberAsyncImagePainter(model = uiState.value.user!!.photo),
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier
+                        .size(120.dp)
+                        .clip(shape = CircleShape)
+                )
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier
+                        .size(120.dp)
+                        .clip(shape = CircleShape)
+                )
+            }
 
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(
-            text = uiState.value.user?.email ?: "", style = MaterialTheme.typography.labelSmall
-        )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = uiState.value.user?.email ?: "", style = MaterialTheme.typography.labelSmall
+            )
 
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(
-            modifier = Modifier.padding(horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = uiState.value.user?.firstname ?: "",
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = uiState.value.user?.lastname ?: "",
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = uiState.value.user?.firstname ?: "",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = uiState.value.user?.lastname ?: "",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
 
-        Spacer(modifier = Modifier.height(26.dp))
-        Button(
-            onClick = { navController.navigate(Screen.UserModify.route) },
-        ) {
-            Text(
-                text = "Modifier mon profil",
-            )
-        }
+            Spacer(modifier = Modifier.height(26.dp))
+            Button(
+                onClick = { navController.navigate(Screen.UserModify.route) },
+            ) {
+                Text(
+                    text = "Modifier mon profil",
+                )
+            }
 
-        Spacer(modifier = Modifier.height(10.dp))
-        Button(
-            onClick = {
-                viewAuthModel.signOut()
-                navController.navigate(Screen.MainScreen.route)
-            },
-        ) {
-            Text(
-                text = "Se déconnecter",
-            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                onClick = {
+                    viewAuthModel.signOut()
+                    navController.navigate(Screen.MainScreen.route)
+                },
+            ) {
+                Text(
+                    text = "Se déconnecter",
+                )
+            }
         }
+    }
+}
+
+@Composable
+fun LoadingIndicator() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator()
     }
 }
