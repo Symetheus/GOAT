98f2d859f7da04713293573f983531d705566e4c
Elsa <elsafirmin@hotmail.fr>
2023-07-18 14:33:26 +0200

    [US-80][UC-81] Get all users with badges for ranking

--- app/src/main/java/com/example/goat/presentation/player/PlayerViewModel.kt
+++ app/src/main/java/com/example/goat/presentation/player/PlayerViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.goat.presentation.player
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.goat.common.Resource
