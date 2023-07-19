32c1b238c4d5ab3bc1a3edaee90a64cee653b96d
Elsa <elsafirmin@hotmail.fr>
2023-07-19 19:59:15 +0200

    [US-70][UC-72] Get all quiz contribution

--- app/src/main/java/com/example/goat/presentation/Screen.kt
+++ app/src/main/java/com/example/goat/presentation/Screen.kt
@@ -8,7 +8,8 @@ sealed class Screen(val route: String) {
     object ListAllPlayer: Screen("list_all_player")
     object DailyQuizScreen: Screen("daly_quiz")
     object QuizScreen: Screen("quiz")
-    object  ContributionQuizScreen: Screen("contribution_quiz")
+    object ContributionQuizScreen: Screen("contribution_quiz")
+    object AddContributionQuizScreen: Screen("add_contribution_quiz")
     object ChallengeScreen: Screen("challenge")
     object RankingWithBadgeScreen: Screen("ranking_with_badge")
 }
