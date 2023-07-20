07e9354117984e3eeb675e3bb7226d3f1c30217b
Elsa <elsafirmin@hotmail.fr>
2023-07-20 15:06:18 +0200

    [US-70][UC-72] Get all quiz contribution

--- app/src/main/java/com/example/goat/domain/interactor/contribution_quiz/ContributionQuizInteractor.kt
+++ app/src/main/java/com/example/goat/domain/interactor/contribution_quiz/ContributionQuizInteractor.kt
@@ -2,9 +2,13 @@ package com.example.goat.domain.interactor.contribution_quiz
 
 import com.example.goat.domain.interactor.contribution_quiz.add_quiz.AddQuizUC
 import com.example.goat.domain.interactor.contribution_quiz.get_quiz.GetQuizUC
+import com.example.goat.domain.interactor.contribution_quiz.generate_quiz_answers.GenerateQuizAnswersUC
+import com.example.goat.domain.interactor.contribution_quiz.get_characters.GetCharactersUC
 import javax.inject.Inject
 
 data class ContributionQuizInteractor @Inject constructor(
     val addQuizUC: AddQuizUC,
     val getQuizUC: GetQuizUC,
+    val generateQuizAnswersUC: GenerateQuizAnswersUC,
+    val getCharactersUC: GetCharactersUC,
 )
--- app/src/main/java/com/example/goat/domain/interactor/gotq/GotqInteractor.kt
+++ app/src/main/java/com/example/goat/domain/interactor/gotq/GotqInteractor.kt
@@ -1,10 +1,8 @@
 package com.example.goat.domain.interactor.gotq
 
-import com.example.goat.domain.interactor.gotq.generate_quiz_answers.GenerateQuizAnswersUC
 import com.example.goat.domain.interactor.gotq.get_characters.GetCharactersUC
 import javax.inject.Inject
 
 data class GotqInteractor @Inject constructor(
     val getCharactersUC: GetCharactersUC,
-    val generateQuizAnswersUC: GenerateQuizAnswersUC,
 )
