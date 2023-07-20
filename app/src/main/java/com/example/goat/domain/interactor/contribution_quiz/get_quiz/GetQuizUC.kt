8d89c826b0ca8ae61dfb38b79b9cc90310bcfad2
Elsa <elsafirmin@hotmail.fr>
2023-07-20 12:51:20 +0200

    [US-70][UC-71] Add quiz with citation and character

--- app/src/main/java/com/example/goat/domain/interactor/contribution_quiz/get_quiz/GetQuizUC.kt
+++ app/src/main/java/com/example/goat/domain/interactor/contribution_quiz/get_quiz/GetQuizUC.kt
@@ -2,17 +2,18 @@ package com.example.goat.domain.interactor.contribution_quiz.get_quiz
 
 import com.example.goat.common.Resource
 import com.example.goat.domain.model.ContributionQuiz
+import com.example.goat.domain.model.Quote
 import com.example.goat.domain.repository.ContributionQuizRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetQuizUC  @Inject constructor(private val contributionQuizRepository: ContributionQuizRepository) {
-    operator fun invoke(): Flow<Resource<List<ContributionQuiz>>> = flow {
+    operator fun invoke(): Flow<Resource<List<Quote>>> = flow {
         try {
             emit(Resource.Loading())
-            val listQuiz = contributionQuizRepository.getQuiz()
-            emit(Resource.Success(listQuiz))
+            val listQuote = contributionQuizRepository.getQuiz()
+            emit(Resource.Success(listQuote))
         } catch (e: Exception) {
             emit(Resource.Error(message = "Error with {${e.localizedMessage}}"))
         }
